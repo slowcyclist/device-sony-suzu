956d98dfbef1b1c5d99bac704142748ab6d583d7
Alfred Neumayer <dev.beidl@gmail.com>
2019-08-20 19:47:39 +0200

    suzu: Multiple changes
    - Support headset detection using the h2w kernel driver and udev rules+script
    - Load additional venus firmware drivers
    - Load pulseaudio-modules-droid-glue-24
    - Ship additional PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -72,10 +72,14 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/system/halium/etc/init/bluetooth-touch-android.conf:system/halium/etc/init/bluetooth-touch-android.conf \
     $(DEVICE_PATH)/rootdir/system/halium/etc/pulse/touch.pa:system/halium/etc/pulse/touch.pa \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/bluetooth-touch/loire:system/halium/usr/share/bluetooth-touch/loire \
-    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android
+    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android \
+    $(DEVICE_PATH)/rootdir/system/halium/usr/share/h2w/switch:system/halium/usr/share/h2w/switch
 
 PRODUCT_PACKAGES += \
-    libubuntu_application_api
+    libubuntu_application_api \
+    libdroidmedia \
+    libui_compat_layer \
+    libsf_compat_layer
 
 # Disable Cyngn additions
 CYNGN_TARGET := false
