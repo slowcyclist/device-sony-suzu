186f5e9cbba0296f56ad0809ac537c0e1c5ac3e4
Alfred Neumayer <dev.beidl@gmail.com>
2019-09-03 17:50:28 +0200

    device: Move venus apparmor override from video.d to graphics.d

--- device.mk
+++ device.mk
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/system/halium/etc/init/bluetooth-touch-android.conf:system/halium/etc/init/bluetooth-touch-android.conf \
     $(DEVICE_PATH)/rootdir/system/halium/etc/pulse/touch.pa:system/halium/etc/pulse/touch.pa \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/bluetooth-touch/loire:system/halium/usr/share/bluetooth-touch/loire \
-    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android \
+    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/h2w/switch:system/halium/usr/share/h2w/switch
 
 PRODUCT_PACKAGES += \
